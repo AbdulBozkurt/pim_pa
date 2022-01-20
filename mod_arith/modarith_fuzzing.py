895cecf4cd7836e14619784c5a1c0c53798ca6a2
Patrick Plewka <patrickplewka@googlemail.com>
2022-01-20 23:09:15 +0100

    Added unit and fuzzing tests for mod_arith

---
+++ mod_arith/modarith_fuzzing.py
@@ -0,0 +1,162 @@
+import random
+from typing import Callable, Tuple
+
+import modarith
+
+
+def _are_mutually_prime(x: int, k: int) -> bool:
+    return True in [(x % i) == 0 and (k % i) == 0 for i in range(2, min(x, k))]
+
+
+class FuzzingTester:
+
+    def __init__(self, min_rand: int, max_rand: int, n: int):
+        self.min_rand = min_rand
+        self.max_rand = max_rand
+        self.n = n
+
+    def run(self):
+
+        functions = (self.test_add, self.test_sub, self.test_mul, self.test_inv, self.test_div, self.test_euclidean,
+                     self.test_extended_euclidean)
+
+        def get_random_value():
+            return random.randint(self.min_rand, self.max_rand)
+        failure_count: int = 0
+        for i in range(self.n):
+            if i % (self.n // 100) == 0 or True:
+                percentage = (i * 100) // self.n
+                print(f"\r[{percentage: >3}%]: run #[{i: >{len(str(self.n))}}/{self.n}] currently", flush=True, end="")
+            x: int = get_random_value()
+            y: int = get_random_value()
+            k: int = get_random_value()
+            func: Callable[[int, int, int], Tuple[Callable, Tuple, int, Exception, str]]
+            for func in functions:
+                result = func(x, y, k)
+                if result is None:
+                    continue
+                failure_count += 1
+                function, params, arithmetic_result, exception, msg = result
+                print(f"\rfunction call {function.__name__}{params} returned {arithmetic_result} with "
+                      f"error {repr(exception)}" +
+                      (f"Additional message: {msg}" if (msg is not None and len(msg) > 0) else ""), flush=True)
+
+        print(f"\rdone with {failure_count} test failures out of {self.n * len(functions)} (n times the number of "
+              f"functions)")
+
+    @staticmethod
+    def test_add(x: int, y: int, k: int):
+        try:
+            result = modarith.mod_add(x, y, k)
+        except ValueError as err:
+            if k <= 1:
+                return
+            else:
+                return modarith.mod_add, (x, y, k), None, err, ""
+        if k <= 1:
+            return modarith.mod_add, (x, y, k), result, None, "expected ValueError, because k <= 1"
+        if result != (x + y) % k:
+            return modarith.mod_add, (x, y, k), result, None, ""
+
+    @staticmethod
+    def test_sub(x: int, y: int, k: int):
+        try:
+            result = modarith.mod_sub(x, y, k)
+        except ValueError as err:
+            if k <= 1:
+                return
+            else:
+                return modarith.mod_sub, (x, y, k), None, err, ""
+        if k <= 1:
+            return modarith.mod_sub, (x, y, k), result, None, "expected ValueError, because k <= 1"
+        if result != (x - y) % k:
+            return modarith.mod_sub, (x, y, k), result, None, ""
+
+    @staticmethod
+    def test_mul(x: int, y: int, k: int):
+        try:
+            result = modarith.mod_mul(x, y, k)
+        except ValueError as err:
+            if k <= 1:
+                return
+            else:
+                return modarith.mod_mul, (x, y, k), None, err, ""
+        if k <= 1:
+            return modarith.mod_mul, (x, y, k), result, None, "expected ValueError, because k <= 1"
+        if result != (x * y) % k:
+            return modarith.mod_mul, (x, y, k), result, None, ""
+
+    # noinspection PyUnusedLocal
+    @staticmethod
+    def test_inv(x: int, y: int, k: int):
+        try:
+            result = modarith.mod_inv(x, k)
+        except ValueError as err:
+            if k <= 1:
+                return
+            if modarith.euclidean_alg(x % k, k) != 1:
+                return
+            return modarith.mod_inv, (x, k), None, err, "not mutually prime"
+        if k <= 1:
+            return modarith.mod_inv, (x, k), result, None, "expected ValueError, because k <= 1"
+        if (result * x) % k != 1:
+            return modarith.mod_inv, (x, k), result, None, "multiplication does not result in 1"
+
+    @staticmethod
+    def test_div(x: int, y: int, k: int):
+        try:
+            result = modarith.mod_div(x, y, k)
+        except ValueError as err:
+            if k <= 1:
+                return
+            if modarith.euclidean_alg(y % k, k) != 1:
+                return
+            return modarith.mod_div, (x, y, k), None, err, ""
+        if k <= 1:
+            return modarith.mod_div, (x, y, k), result, None, "expected ValueError, because k <= 1"
+        if (result * y) % k != x % k:
+            return modarith.mod_div, (x, y, k), result, None, "result * y mod k was not x mod k"
+
+    # noinspection PyUnusedLocal
+    @staticmethod
+    def test_euclidean(x: int, y: int, k: int):
+        try:
+            result = modarith.euclidean_alg(x, y)
+        except ValueError as err:
+            if x < 0 or y < 0:
+                return
+            return modarith.euclidean_alg, (x, y), None, err, "ValueError despite both x and y >= 0"
+        if x < 0 or y < 0:
+            return modarith.euclidean_alg, (x, y), result, None, "expected ValueError, because x or y < 0"
+        for i in range(result + 1, int(min(x, y) ** .5) + 1):
+            if x % i == 0 and y % i == 0:
+                return modarith.euclidean_alg, (x, y), result, None, f"was not gcd. {i} is greater common divisor"
+
+    # noinspection PyUnusedLocal
+    @staticmethod
+    def test_extended_euclidean(x: int, y: int, k: int):
+        try:
+            result = modarith.extended_euclidean_alg(x, y)
+        except ValueError as err:
+            if x < 0 or y < 0:
+                return
+            return modarith.extended_euclidean_alg, (x, y), None, err, "ValueError despite both x and y >= 0"
+        if x < 0 or y < 0:
+            return modarith.extended_euclidean_alg, (x, y), result, None, "expected ValueError, because x or y < 0"
+        if not isinstance(result, tuple) or len(result) != 3:
+            return modarith.extended_euclidean_alg, (x, y), result, None, "result must be a tuple of length 3"
+        d, s, t = result
+        if d != s * x + t * y:
+            return modarith.extended_euclidean_alg, (x, y), result, None, "result must satisfy the equation d == s * " \
+                                                                          "x + t * y "
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("min_rand", type=int, help="the smallest possible random value")
+    parser.add_argument("max_rand", type=int, help="the biggest possible random value")
+    parser.add_argument("n", type=int, help="the amount of test runs")
+    args = parser.parse_args()
+    tester = FuzzingTester(int(args.min_rand), int(args.max_rand), int(args.n))
+    tester.run()
---
+++ mod_arith/modarith_unittests.py
@@ -0,0 +1,95 @@
+import unittest
+from modarith import *
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_add_success(self):
+        result = mod_add(35, 42, 11)
+        self.assertEqual(0, result)
+
+    def test_sub_success(self):
+        result = mod_sub(35, 42, 11)
+        self.assertEqual(4, result)
+
+    def test_mul_success(self):
+        result = mod_mul(5, 3, 11)
+        self.assertEqual(4, result)
+
+    def test_div_success(self):
+        result = mod_div(5, 3, 11)
+        self.assertEqual(9, result)
+
+    def test_inv_success(self):
+        result = mod_inv(8, 11)
+        self.assertEqual(7, result)
+
+    def test_error_zero_k(self):
+        functions = (mod_add, mod_sub, mod_div, mod_mul)
+        for func in functions:
+            try:
+                func(5, 4, 0)
+            except ValueError:
+                continue
+            self.fail(f"Test of function {func.__name__} should fail, because k = 0 is not allowed")
+
+    def test_error_zero_k_inv(self):
+        try:
+            mod_inv(5, 0)
+        except ValueError:
+            return
+        self.fail("Test of function mod_inv should fail, because k = 0 is not allowed")
+
+    def test_error_negative_k(self):
+        functions = (mod_add, mod_sub, mod_div, mod_mul)
+        for func in functions:
+            try:
+                func(5, 4, -1)
+            except ValueError:
+                continue
+            self.fail(f"Test of function {func.__name__} should fail, because k < 0 is not allowed")
+
+    def test_error_negative_k_inv(self):
+        try:
+            mod_inv(5, -1)
+        except ValueError:
+            return
+        self.fail("Test of function mod_inv should fail, because k < 0 is not allowed")
+
+    def test_error_inv_not_mutual_prime(self):
+        try:
+            mod_inv(5, 15)
+        except ValueError:
+            return
+        self.fail("Test of function mod_inv should fail, because x and k are not mutually prime")
+
+    def test_error_negative_euclidean(self):
+        params = ((-8, -5), (-7, 4), (15, -4))
+        for param in params:
+            try:
+                euclidean_alg(*param)
+            except ValueError:
+                continue
+            self.fail(f"Test of function euclidean_alg should fail with parameters {param},"
+                      " because they are not allowed to be negative")
+
+    def test_euclidean_success(self):
+        x = 127
+        y = 532
+        gcd = euclidean_alg(x, y)
+        self.assertTrue((x % gcd) == 0, f"{gcd} was result, but does not divide {x}")
+        self.assertTrue((y % gcd) == 0, f"{gcd} was result, but does not divide {y}")
+        smaller_one = min(x, y)
+        for i in range(gcd + 1, smaller_one):
+            self.assertTrue(((x % i) != 0) or ((y % i) != 0), f"gcd {gcd} was not the greatest divisor. {i} is greater")
+
+    def test_ext_euclidean_success(self):
+        x = 127
+        y = 532
+        d, s, t = extended_euclidean_alg(x, y)
+        self.assertTrue(d == s * x + t * y, f"results d = {d}, s = {s} and t = {t} did not satisfy the equation "
+                                            f"d == s * x + t * y with x = {x} and y = {y}")
+
+
+if __name__ == '__main__':
+    unittest.main()
