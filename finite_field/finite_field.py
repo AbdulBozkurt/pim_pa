f7966a064de7b5bdbbbfda8e32bf7cc1d17f9046
LaurinReinig <laurin.reinig@gmail.com>
2022-01-23 19:45:38 +0100

    added class finite_field

---
+++ finite_field/finite_field.py
@@ -0,0 +1,17 @@
+import prime_numbers
+
+
+class FiniteField:
+
+    def __init__(self, p: int):
+        if not prime_numbers.prime_test(p):
+            raise ValueError('The given base {0} is not a prime number.'.format(p))
+        self.p = p
+
+    def __str__(self):
+        return "Base: {0}".format(self.p)
+
+    def __eq__(self, other):
+        return self.p == other.p
+
+
--- finite_field/finite_field_element.py
+++ finite_field/finite_field_element.py
@@ -1,50 +1,56 @@
 import mod_arith.modarith as mod
-import prime_numbers
+from finite_field import FiniteField
 
 
 class FiniteFieldElement:
 
-    def __init__(self, e=0, p=0):
+    def __init__(self, e=0, field=None):
+        if type(e) != int:
+            raise ValueError('The given element object is not of type int')
         self.e = e
-        if not prime_numbers.prime_test(p):
-            raise ValueError('The given base {0} is not a prime number.'.format(p))
-        self.p = p
+        if type(field) != FiniteField:
+            raise ValueError('The given field object is not of type finite field')
+        self.field = field
 
     def __str__(self):
-        return "(Element: {0}, Base: {1})".format(self.e, self.p)
+        return "(Element: {0}, {1})".format(self.e, self.field)
 
     def __add__(self, other):
-        if self.p != other.p:
+        if self.field != other.field:
             raise ArithmeticError('Cannot add two elements from different finite fields. '
-                                  'Given bases were {0} and {1}'.format(self.p, other.p))
-        e = mod.mod_add(self.e, other.e, self.p)
-        return FiniteFieldElement(e, self.p)
+                                  'Given bases were {0} and {1}'.format(self.field, other.field))
+        e = mod.mod_add(self.e, other.e, self.field.p)
+        return FiniteFieldElement(e, self.field)
 
     def __sub__(self, other):
-        if self.p != other.p:
+        if self.field != other.field:
             raise ArithmeticError('Cannot subtract two elements from different finite fields. '
-                                  'Given bases were {0} and {1}'.format(self.p, other.p))
-        e = mod.mod_sub(self.e, other.e, self.p)
-        return FiniteFieldElement(e, self.p)
+                                  'Given bases were {0} and {1}'.format(self.field, other.field))
+        e = mod.mod_sub(self.e, other.e, self.field.p)
+        return FiniteFieldElement(e, self.field)
 
     def __mul__(self, other):
-        if self.p != other.p:
+        if self.field != other.field:
             raise ArithmeticError('Cannot multiply two elements from different finite fields. '
-                                  'Given bases were {0} and {1}'.format(self.p, other.p))
-        e = mod.mod_mul(self.e, other.e, self.p)
-        return FiniteFieldElement(e, self.p)
+                                  'Given bases were {0} and {1}'.format(self.field, other.field))
+        e = mod.mod_mul(self.e, other.e, self.field.p)
+        return FiniteFieldElement(e, self.field)
 
     def __truediv__(self, other):
-        if self.p != other.p:
+        if self.field != other.field:
             raise ArithmeticError('Cannot divide two elements from different finite fields. '
-                                  'Given bases were {0} and {1}'.format(self.p, other.p))
-        e = mod.mod_div(self.e, other.e, self.p)
-        return FiniteFieldElement(e, self.p)
+                                  'Given bases were {0} and {1}'.format(self.field, other.field))
+        e = mod.mod_div(self.e, other.e, self.field.p)
+        return FiniteFieldElement(e, self.field)
+
+    def inv(self):
+        return FiniteFieldElement(1, self.field)/self
 
 
 if __name__ == '__main__':
-    e1 = FiniteFieldElement(10, 11)
-    e2 = FiniteFieldElement(3, 11)
+    e1 = FiniteFieldElement(10, FiniteField(11))
+    e2 = FiniteFieldElement(3, FiniteField(11))
+    print(e2.inv())
     print(e1+e2)
     print(e1-e2)
     print(e1*e2)
