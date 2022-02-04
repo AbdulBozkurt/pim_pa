2bc6e1c038a4b06396e519c9d827f8eeafd68a24
AbdulBozkurt <abdul890@hotmail.de>
2022-02-04 18:04:15 +0100

    added addition and doubling of points

--- elliptic_curve/elliptic_curve.py
+++ elliptic_curve/elliptic_curve.py
@@ -1,16 +1,17 @@
 import finite_field.finite_field_element as field
+from finite_field.finite_field_element import FiniteFieldElement
 from finite_field.finite_field import FiniteField
-import point_element as point
+from point_element import PointElement
 from algorithm import get_daa_bits, get_naf_bits
 
 
 class EllipticCurve:
 
-    def __init__(self, a: field. FiniteFieldElement, b: field.FiniteFieldElement):
+    def __init__(self, a: FiniteFieldElement, b: FiniteFieldElement):
 
-        e1 = field.FiniteFieldElement(-1, a.field)
-        e2 = field.FiniteFieldElement(4, a.field)
-        e3 = field.FiniteFieldElement(27, a.field)
+        e1 = FiniteFieldElement(-1, a.field)
+        e2 = FiniteFieldElement(4, a.field)
+        e3 = FiniteFieldElement(27, a.field)
         determinant = e1 * (e2 * (a * a * a) + e3 * (b * b))
 
         if determinant.e == 0:
@@ -22,12 +23,24 @@ class EllipticCurve:
     def __str__(self):
         return "(Curve: y^2 = x^3 + {0}x + {1})".format(self.a.e, self.b.e)
 
-    def double_and_add(self, n: int, p: point.PointElement):
-        # TODO: implement addition and doubling in point-class
+    def is_on_curve(self, p: PointElement):
+        e1 = FiniteFieldElement(2, p.x.field)
+        e2 = FiniteFieldElement(3, p.x.field)
 
-        e1 = field.FiniteFieldElement(0, self.a)
-        e2 = field.FiniteFieldElement(0, self.a)
-        result = point.PointElement(e1, e2)
+        left = p.y * p.y
+        right = p.x * p.x * p.x + e1 * p.x + e2
+
+        return left == right
+
+    def scalar_mul(self, n: int, p: PointElement):
+        return
+
+    def double_and_add(self, n: int, p: PointElement):
+        # TODO: check if point is on curve
+
+        e1 = FiniteFieldElement(0, self.a)
+        e2 = FiniteFieldElement(0, self.a)
+        result = PointElement(e1, e2)
         tmp = p
 
         for bit in get_daa_bits(n):
@@ -37,12 +50,12 @@ class EllipticCurve:
 
         return result
 
-    def non_adjacent_form(self, n: int, p: point.PointElement):
+    def non_adjacent_form(self, n: int, p: PointElement):
         # TODO: implement addition and doubling in point-class
 
         e1 = field.FiniteFieldElement(0, self.a)
         e2 = field.FiniteFieldElement(0, self.a)
-        result = point.PointElement(e1, e2)
+        result = PointElement(e1, e2)
         tmp = p
 
         for bit in get_naf_bits(n):
@@ -54,14 +67,14 @@ class EllipticCurve:
 
 
 if __name__ == '__main__':
-    param_a = field.FiniteFieldElement(2, FiniteField(17))
-    param_b = field.FiniteFieldElement(3, FiniteField(17))
+    param_a = FiniteFieldElement(2, FiniteField(17))
+    param_b = FiniteFieldElement(3, FiniteField(17))
     weierstrass = EllipticCurve(param_a, param_b)
     print(weierstrass)
 
-    element1 = field.FiniteFieldElement(5, FiniteField(17))
-    element2 = field.FiniteFieldElement(4, FiniteField(17))
-    point1 = point.PointElement(element1, element2)
+    element1 = FiniteFieldElement(5, FiniteField(17))
+    element2 = FiniteFieldElement(4, FiniteField(17))
+    point1 = PointElement(element1, element2)
 
     # weierstrass.double_and_add(20, point1)
     # weierstrass.non_adjacent_form(20, point1)
--- elliptic_curve/point_element.py
+++ elliptic_curve/point_element.py
@@ -1,35 +1,54 @@
-import finite_field.finite_field_element as field
+from finite_field.finite_field_element import FiniteFieldElement
+from finite_field.finite_field import FiniteField
 
 
 class PointElement:
 
-    def __init__(self, x: field.FiniteFieldElement, y: field.FiniteFieldElement):
+    def __init__(self, x: FiniteFieldElement, y: FiniteFieldElement):
 
-        if not x.p == y.p:
+        if x.field != y.field:
             raise ValueError('The bases of  the given elements are not equal.')
-
         self.x = x
         self.y = y
+        # TODO: inherit A from elliptic curve
+        self.a = FiniteFieldElement(2, x.field)
 
     def __str__(self):
         return "(Point: ({0}|{1}) )".format(self.x.e, self.y.e)
 
-    def addition(self, other, a):
-        # TODO: eea for inverse of elements
+    def __add__(self, other):
+        # TODO: use this or the formulas from http://hyperelliptic.org ?
+        if self.x != other.x:
+            slope = (other.y - self.y) / (other.x - self.x)
+            x3 = slope * slope - self.x - other.x
+            y3 = slope * (self.x - x3) - self.y
+            return PointElement(x3, y3)
 
-        if self:
-            # x
-            # slope 1:  y2 - y1 / x2 - x1
-            # slope 2:  3 * x1^2 + A / 2 * y1
-            return "addition"
-        elif other:
-            return "doubling"
+        elif self == other and self.y != 0:
+            e1 = FiniteFieldElement(3, self.x.field)
+            e2 = FiniteFieldElement(2, self.x.field)
+            slope = (e1 * self.x * self.x + self.a) / (e2 * self.y)
+            x3 = slope * slope - self.x - self.x
+            y3 = slope * (self.x - x3) - self.y
+            return PointElement(x3, y3)
         else:
-            return "point at infinity"
+            # TODO: rather raising error or handle infinity with projective coordinates ?
+            raise ValueError('Point at Infinity')
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __neg__(self):
+        i = FiniteFieldElement(-1, self.y.field)
+        return PointElement(self.x, self.y * i)
 
 
 if __name__ == '__main__':
-    element1 = field.FiniteFieldElement(3, 17)
-    element2 = field.FiniteFieldElement(5, 17)
+    element1 = FiniteFieldElement(3, FiniteField(17))
+    element2 = FiniteFieldElement(5, FiniteField(17))
     p1 = PointElement(element1, element2)
+    p2 = PointElement(element2, element1)
     print(p1)
+    print(-p1)
+    print(p1+p2)
+    print(p1-p2)
