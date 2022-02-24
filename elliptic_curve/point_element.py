0b36ff8c2d9dd0aefd290ca4385ab99e147f603c
AbdulBozkurt <abdul890@hotmail.de>
2022-02-24 19:41:32 +0100

    added projective coordinate for point at infinity and added daa algorithm

--- elliptic_curve/point_element.py
+++ elliptic_curve/point_element.py
@@ -5,7 +5,7 @@ from algorithm import get_daa_bits, get_naf_bits
 
 
 class PointElement:
-    def __init__(self, x: FiniteFieldElement, y: FiniteFieldElement, curve: EllipticCurve):
+    def __init__(self, x: FiniteFieldElement, y: FiniteFieldElement, z: FiniteFieldElement, curve: EllipticCurve):
         """Initializes an PointElement by providing it with two (later three) coordinates of type
         FiniteFieldElement and the corresponding curve. Checks first, whether the fields of the
         given elements are equal. If they are not equal, then raise ValueError."""
@@ -14,10 +14,11 @@ class PointElement:
             raise ValueError('The bases of  the given elements are not equal.')
         self.x = x
         self.y = y
+        self.z = z
         self.curve = curve
 
     def __str__(self):
-        return "(Point: ({0}|{1}) )".format(self.x.e, self.y.e)
+        return "(Point: ({0}:{1}:{2}))".format(self.x.e, self.y.e, self.z.e)
 
     def __add__(self, other):
         """Adds two points to retrieve a new one.
@@ -26,24 +27,37 @@ class PointElement:
         # if not self.is_on_curve() or not other.is_on_curve():
         #     raise ValueError('One or more points are not on the curve.')
 
+        # handles addition with Point at Infinity
+        if self.z.e == 0 and other.z.e == 0:
+            self.x.e = 0
+            self.y.e = 1
+            self.z.e = 0
+            return self
+        elif self.z != other.z:
+            if other.z.e == 0:
+                return self
+            else:
+                return other
+
         # TODO: use this or the formulas from http://hyperelliptic.org ?
         # TODO: check which formulas are the most efficient
         if self.x != other.x:
             slope = (other.y - self.y) / (other.x - self.x)
             x3 = slope * slope - self.x - other.x
             y3 = slope * (self.x - x3) - self.y
-            return PointElement(x3, y3, self.curve)
-
+            return PointElement(x3, y3, self.z, self.curve)
         elif self == other and self.y != 0:
             e1 = FiniteFieldElement(3, self.x.field)
             e2 = FiniteFieldElement(2, self.x.field)
             slope = (e1 * self.x * self.x + self.curve.a) / (e2 * self.y)
             x3 = slope * slope - self.x - self.x
             y3 = slope * (self.x - x3) - self.y
-            return PointElement(x3, y3, self.curve)
+            return PointElement(x3, y3, self.z, self.curve)
         else:
-            # TODO: rather raising error or handle infinity with projective coordinates ?
-            raise ValueError('Point at Infinity')
+            self.x.e = 0
+            self.y.e = 1
+            self.z.e = 0
+            return self
 
     def __sub__(self, other):
         """Calculates the subtraction of two points."""
@@ -52,19 +66,21 @@ class PointElement:
     def __neg__(self):
         """Calculates the negation of a point by multiplying its y-coordinate with -1."""
         i = FiniteFieldElement(-1, self.y.field)
-        return PointElement(self.x, self.y * i, self.curve)
+        return PointElement(self.x, self.y * i, self.z, self.curve)
 
     def __eq__(self, other):
         """Checks, whether the point is equal to another one."""
-        return self.x == other.x and self.y == other.y and self.curve == other.curve
+        return self.x == other.x and self.y == other.y and self.z == other.z and self.curve == other.curve
 
     def is_on_curve(self):
         """Checks, whether the point is on its elliptic curve, by inserting them
         into the curve. If both sides of the equation are not equal,
         then raise ValueError."""
         # TODO: eq-method in finite_field_element?
-        left = self.y * self.y
-        right = self.x * self.x * self.x + self.curve.a * self.x + self.curve.b
+        left = self.y * self.y * self.z
+        right = self.x * self.x * self.x \
+                + self.curve.a * self.x * self.z * self.z \
+                + self.curve.b * self.z * self.z * self.z
         return left.field == right.field and left.e == right.e
 
     def scalar_mul(self, n: int):
@@ -74,7 +90,7 @@ class PointElement:
         #     raise ValueError('The given point is not on the curve.')
 
         tmp = self
-        for i in range(n-1):
+        for i in range(n - 1):
             tmp = tmp + self
         return tmp
 
@@ -86,8 +102,9 @@ class PointElement:
         #     raise ValueError('The given point is not on the curve.')
 
         e1 = FiniteFieldElement(0, self.x.field)
-        e2 = FiniteFieldElement(0, self.y.field)
-        result = PointElement(e1, e2, self.curve)   # needs to be Point at Infinity
+        e2 = FiniteFieldElement(1, self.y.field)
+        e3 = FiniteFieldElement(0, self.y.field)
+        result = PointElement(e1, e2, e3, self.curve)
         tmp = self
 
         for i in get_daa_bits(n):
@@ -134,16 +151,17 @@ if __name__ == '__main__':
 
     element1 = FiniteFieldElement(4, FiniteField(13))
     element2 = FiniteFieldElement(7, FiniteField(13))
-    p1 = PointElement(element1, element2, curve1)
+    element3 = FiniteFieldElement(1, FiniteField(13))
+    p1 = PointElement(element1, element2, element3, curve1)
     p2 = p1 + p1
 
     print("P1 on curve: %s" % p1.is_on_curve())
     print("P2 on curve: %s" % p2.is_on_curve())
     print(" P1: %s" % p1)
     print("-P1: %s" % -p1)
-    print("P1+P1: %s" % (p1+p1))
-    print("P1+P2: %s" % (p1+p2))
-    print("P1-P2: %s" % (p1-p2))
+    print("P1+P1: %s" % (p1 + p1))
+    print("P1+P2: %s" % (p1 + p2))
+    print("P1-P2: %s" % (p1 - p2))
     print("%s*P1 (scalar): %s" % (9, p1.scalar_mul(9)))
-    # print("%s*P1 (daa): %s" % (9, p1.double_and_add(9)))
+    print("%s*P1 (daa): %s" % (9, p1.double_and_add(9)))
     # print("%s*P1 (naf): %s" % (9, p1.non_adjacent_form(9)))
