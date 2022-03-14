c6011d8722451cb343336aba3e5a4dfa2a5fdfbd
Laurin Reinig <34340216+LaurinReinig@users.noreply.github.com>
2022-03-14 15:45:03 +0100

    Implemented polynomials in finitefieldelement

--- finite_field/finite_field_element.py
+++ finite_field/finite_field_element.py
@@ -5,47 +5,81 @@ __all__ = ["FiniteFieldElement"]
 
 class FiniteFieldElement:
 
-    def __init__(self, e=0, field=None):
-        if type(e) != int:
-            raise ValueError('The given element object is not of type int')
-        self.e = e
-        if type(field) != FiniteField:
+    def __init__(self, e=[], field=None):
+        if not isinstance(e, list):
+            raise ValueError('The given element object is not of type list')
+        if not isinstance(field, FiniteField):
             raise ValueError('The given field object is not of type finite field')
         self.field = field
+        self.e = field.reduce_poly(e)
 
     def __str__(self):
+        # TODO for polynomial
         return "(Element: {0}, {1})".format(self.e, self.field)
 
     def __add__(self, other):
+        if not isinstance(other, FiniteFieldElement):
+            raise ValueError('Tried to add a object that is not of type FiniteFieldElement but of type: {0}'
+                             .format(type(other)))
         if self.field != other.field:
             raise ArithmeticError('Cannot add two elements from different finite fields. '
                                   'Given bases were {0} and {1}'.format(self.field, other.field))
-        e = mod.mod_add(self.e, other.e, self.field.p)
-        return FiniteFieldElement(e, self.field)
+        a = self.e
+        b = other.e
+        if len(a) < len(b):
+            temp = b.copy()
+            b = a.copy()
+            a = temp
+        b = list(reversed(b))
+        for i in range(len(a) - len(b)):
+            b.append(0)
+        result = list()
+        for i, e in enumerate(reversed(b)):
+            result.append(mod.mod((e + a[i]), self.field.p))
+        return FiniteFieldElement(result, self.field)
 
     def __sub__(self, other):
+        if not isinstance(other, FiniteFieldElement):
+            raise ValueError('Tried to add a object that is not of type FiniteFieldElement but of type: {0}'
+                             .format(type(other)))
+        if self.field != other.field:
+            raise ArithmeticError('Cannot add two elements from different finite fields. '
+                                  'Given bases were {0} and {1}'.format(self.field, other.field))
+        a = self.e
+        b = other.e
+        if len(a) < len(b):
+            temp = b.copy()
+            b = a.copy()
+            a = temp
+        b = list(reversed(b))
+        for i in range(len(a) - len(b)):
+            b.append(0)
+        result = list()
+        for i, e in enumerate(reversed(b)):
+            result.append(mod.mod((e - a[i]), self.field.p))
+        return FiniteFieldElement(result, self.field)
+
+    def __mul__(self, other):
+        if not isinstance(other, FiniteFieldElement):
+            raise ValueError('Tried to add a object that is not of type FiniteFiledElement but of type: {0}'
+                             .format(type(other)))
         if self.field != other.field:
             raise ArithmeticError('Cannot subtract two elements from different finite fields. '
                                   'Given bases were {0} and {1}'.format(self.field, other.field))
-        e = mod.mod_sub(self.e, other.e, self.field.p)
-        return FiniteFieldElement(e, self.field)
-
-    def __mul__(self, other):
-        if self.field != other.field:
-            raise ArithmeticError('Cannot multiply two elements from different finite fields. '
-                                  'Given bases were {0} and {1}'.format(self.field, other.field))
-        e = mod.mod_mul(self.e, other.e, self.field.p)
-        return FiniteFieldElement(e, self.field)
+        # initialize result polynomial-array with length a + b
+        result = [0 for x in range(len(self.e) + len(other.e) - 1)]
+        for i, a_element in enumerate(self.e):
+            for j, b_element in enumerate(other.e):
+                result[i + j] += a_element * b_element
+        return FiniteFieldElement(result, self.field)
 
     def __truediv__(self, other):
+        # TODO probably dont need it anymore
         if self.field != other.field:
             raise ArithmeticError('Cannot divide two elements from different finite fields. '
                                   'Given bases were {0} and {1}'.format(self.field, other.field))
         e = mod.mod_div(self.e, other.e, self.field.p)
         return FiniteFieldElement(e, self.field)
-
-    def inv(self):
-        return FiniteFieldElement(1, self.field)/self
 
     def __eq__(self, other):
         if isinstance(other, int):
