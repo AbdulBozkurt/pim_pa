2816189c3ff430ec3f77236bc8b632798865f1f6
Laurin Reinig <34340216+LaurinReinig@users.noreply.github.com>
2022-03-25 20:02:05 +0100

    updated alice and bob with the new curve

--- public_constants.py
+++ public_constants.py
@@ -13,19 +13,21 @@ verbose = True
 
 field = get_safe_field()
 
-a = 340282366762482138434845932244680310780  # curve-parameter a
-b = 308990863222245658030922601041482374867  # curve-parameter b
-# a_arr = generate_poly({}, field.p)
-# b_arr = generate_poly({}, field.p)
-curve_param_a = FiniteFieldElement([a], field)
-curve_param_b = FiniteFieldElement([b], field)
+a = generate_poly({4: 1}, field.p)  # curve-parameter a
+b = generate_poly({6: 1}, field.p)  # curve-parameter b
+curve_param_a = FiniteFieldElement(a, field)
+curve_param_b = FiniteFieldElement(b, field)
 curve1 = EllipticCurve(curve_param_a, curve_param_b)
 
-e1 = 29408993404948928992877151431649155974  # x-coordinate
-e2 = 275621562871047521857442314737465260675  # y-coordinate
-e3 = 1  # z-coordinate
-element1 = FiniteFieldElement([e1], field)
-element2 = FiniteFieldElement([e2], field)
-element3 = FiniteFieldElement([e3], field)
+#   Generator of the Elliptic Curve
+e1 = generate_poly({18: 25, 17: 8, 16: 31, 15: 62, 14: 36, 13: 66, 12: 48, 11: 44, 10: 70,
+                    9: 89, 8: 40, 7: 28, 6: 1, 5: 2, 4: 16, 3: 58, 2: 29, 1: 12, 0: 24}, field.p)
+e2 = generate_poly({18: 71, 17: 15, 16: 98, 15: 41, 14: 85, 13: 6, 12: 6, 11: 62, 10: 63,
+                    9: 71, 8: 87, 7: 34, 6: 68, 5: 38, 4: 58, 3: 2, 2: 16, 1: 8, 0: 70}, field.p)
+e3 = generate_poly({0: 1}, field.p)
+element1 = FiniteFieldElement(e1, field)
+element2 = FiniteFieldElement(e2, field)
+element3 = FiniteFieldElement(e3, field)
 p1 = PointElement(element1, element2, element3, curve1)
-sub_group_size = 340282366762482138443322565580356624661
+
+sub_group_size = 175350605307710078811724140841853552259
